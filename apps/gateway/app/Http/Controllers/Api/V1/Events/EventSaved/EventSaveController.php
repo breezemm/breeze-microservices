f271c2b3f4fd83ecd9319cc09c96703dd3d054fa
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-10-08 22:36:14 +0630

    refactor: change controllers name base on the domain

--- apps/gateway/app/Http/Controllers/Api/V1/Events/EventSaved/EventSaveController.php
+++ apps/gateway/app/Http/Controllers/Api/V1/Events/EventSaved/EventSaveController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Controllers\Api\V1\Events\Saved;
+namespace App\Http\Controllers\Api\V1\Events\EventSaved;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\V1\EventResource;
