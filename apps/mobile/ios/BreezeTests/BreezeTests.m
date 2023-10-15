a5e461df7da74f74dfa3ff6fd2672861219cd32c
minkgkyaw <minkgkyaw9899@gmail.com>
2023-10-15 15:06:54 +0630

    feat(mobile): create mobile project with pnpm and used new architecture

--- apps/mobile/ios/BreezeTests/BreezeTests.m
+++ apps/mobile/ios/BreezeTests/BreezeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface mobileTests : XCTestCase
+@interface BreezeTests : XCTestCase
 
 @end
 
-@implementation mobileTests
+@implementation BreezeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
