c544d7b81fac22d9440221833a4523d37b692a22
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-10-25 23:27:50 +0630

    refactor: start.sh to serve docker

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 echo -e "Confluent Platform is Started ☁️"
 
-docker-compose up -d
+docker-compose up  -d
 
 echo -e "Waiting for Kafka to be ready ⏳"
 docker exec -i kafka /usr/bin/kafka-topics --bootstrap-server kafka:9092 --list
