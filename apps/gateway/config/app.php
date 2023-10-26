bf416578433767d298b27d3c1bc7ade18a1cc75d
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-10-26 11:02:08 +0630

    chore(deps): add horizon

--- apps/gateway/config/app.php
+++ apps/gateway/config/app.php
@@ -168,6 +168,7 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
+        App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Spatie\Permission\PermissionServiceProvider::class,
         Laravel\Passport\PassportServiceProvider::class
