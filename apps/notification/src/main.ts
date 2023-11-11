1fcfbf38ea4ee8c4039d82eeb78177c3e98e22a4
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-11-11 23:12:00 +0630

    chore(ci): add health check in compose

--- apps/notification/src/main.ts
+++ apps/notification/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
       transport: Transport.KAFKA,
       options: {
         client: {
-          brokers: ['localhost:29092'],
+          brokers: ['kafka:29092'],
         },
         parser: {
           keepBinary: true,
