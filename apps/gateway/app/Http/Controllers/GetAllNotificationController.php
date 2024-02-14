b5b060a06ee860279d638005d0458bbd2545dc01
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-02-15 03:38:59 +0630

    feat: add pages

--- apps/gateway/app/Http/Controllers/GetAllNotificationController.php
+++ apps/gateway/app/Http/Controllers/GetAllNotificationController.php
@@ -2,16 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
 class GetAllNotificationController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request)
     {
         $userId = auth()->id();
+        $page = $request->input('page', 1);
 
         $response = Http::notification()
-            ->post('/notifications', [
+            ->post('/notifications?page=' . $page, [
                 'user_id' => $userId,
             ]);
 
