bb10490fa03f74c953cedef561c1bd119ecdec4b
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-02-14 22:56:47 +0630

    refactor: email notification job and mail

--- apps/gateway/app/Mail/EmailVerificationCodeSentMail.php
+++ apps/gateway/app/Mail/EmailVerificationCodeSentMail.php
@@ -8,7 +8,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class EmailVerificationCodeSent extends Mailable
+class EmailVerificationCodeSentMail extends Mailable
 {
     use Queueable, SerializesModels;
 
