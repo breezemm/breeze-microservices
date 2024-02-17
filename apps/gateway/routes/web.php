08028c0814bb93604d59721e53c3a55d32c704d1
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-02-17 10:42:08 +0630

    refactor: remove dead code

--- apps/gateway/routes/web.php
+++ apps/gateway/routes/web.php
@@ -4,30 +4,3 @@ use Illuminate\Support\Facades\Route;
 use Junges\Kafka\Facades\Kafka;
 use Junges\Kafka\Message\Message;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    $message = new Message(body: createKafkaPayload(
-        topic: 'wallets',
-        pattern: [
-            'cmd' => 'checkout',
-        ],
-        data: [
-            'user_id' => 1,
-        ],
-    ));
-    Kafka::publishOn('wallets.created')
-        ->withMessage($message)
-        ->send();
-
-    return view('welcome');
-});
--- apps/gateway/scripts/fresh.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash sh
-docker compose exec gateway php artisan migrate:fresh --seed
-docker compose exec gateway php artisan passport:install
-docker compose exec gateway php artisan storage:link
