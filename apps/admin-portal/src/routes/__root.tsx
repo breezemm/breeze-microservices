6e7a49f86da6649ec3992361a8a66e73b1d1a88e
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-02-19 20:32:10 +0630

    fix: tailwind router config

--- apps/admin-portal/src/routes/__root.tsx
+++ apps/admin-portal/src/routes/__root.tsx
@@ -1,6 +1,17 @@
 import * as React from 'react'
 import {Link, Outlet, createRootRoute} from '@tanstack/react-router'
-import {TanStackRouterDevtools} from '@tanstack/router-devtools'
+import {Suspense} from "react";
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+      // Lazy load in development
+      import('@tanstack/router-devtools').then((res) => ({
+        default: res.TanStackRouterDevtools,
+      })),
+    )
+
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -12,7 +23,7 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
-      <div className="p-2 flex gap-2 text-lg">
+      <div className="p-2 flex gap-4 text-lg">
         <Link
           to="/"
           activeProps={{
@@ -50,7 +61,9 @@ function RootComponent() {
       <hr/>
       <Outlet/>
       {/* Start rendering router matches */}
-      <TanStackRouterDevtools position="bottom-right"/>
+      <Suspense>
+        <TanStackRouterDevtools position={"bottom-right"}/>
+      </Suspense>
     </>
   )
 }
