68a02b94da381574a20744090e7b3147ddbb3f35
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-02-20 15:01:53 +0630

    chore: remove ui pkg

--- research/web/app/layout.tsx
+++ research/web/app/layout.tsx
@@ -1,8 +1,4 @@
-import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Turborepo',
@@ -12,7 +8,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
