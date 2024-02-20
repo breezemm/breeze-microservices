9effe419e95a27c40c5992760e0657a508c92bad
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-02-20 11:24:49 +0630

    feat: add tanstack query

--- apps/web-admin-portal/src/routes/__root.tsx
+++ apps/web-admin-portal/src/routes/__root.tsx
@@ -1,5 +1,27 @@
 import {createRootRoute, Link, Outlet} from '@tanstack/react-router'
-import {TanStackRouterDevtools} from '@tanstack/router-devtools'
+import React, {Suspense} from "react";
+
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+      // Lazy load in development
+      import('@tanstack/router-devtools').then((res) => ({
+        default: res.TanStackRouterDevtools,
+        // For Embedded Mode
+        // default: res.TanStackRouterDevtoolsPanel
+      })),
+    )
+
+const ReactQueryDevtools = process.env.NODE_ENV === 'production'
+  ? () => null // Render nothing in production
+  : React.lazy(() =>
+    // Lazy load in development
+    import('@tanstack/react-query-devtools').then((res) => ({
+      default: res.ReactQueryDevtools,
+    })),
+  )
 
 export const Route = createRootRoute({
   component: () => (
@@ -8,13 +30,19 @@ export const Route = createRootRoute({
         <Link to="/" className="[&.active]:font-bold">
           Home
         </Link>{' '}
+        <Link to={"/posts"} className="[&.active]:font-bold">
+          Posts
+        </Link>{' '}
         <Link to={"/about"} className="[&.active]:font-bold">
           About
         </Link>
       </div>
       <hr/>
       <Outlet/>
-      <TanStackRouterDevtools/>
+      <Suspense fallback={null}>
+        <TanStackRouterDevtools/>
+        <ReactQueryDevtools initialIsOpen={false}/>
+      </Suspense>
     </>
   ),
 })
