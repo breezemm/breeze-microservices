9fad54288fe90ca8cf9bd94423b06392663737f5
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-02-22 05:01:28 +0630

    fix: react auth linter

--- packages/react-auth/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: [require.resolve('@breeze/lint/node')],
-  parserOptions: {
-    project: `${__dirname}/tsconfig.json`,
-  },
-};
--- toolings/eslint-config/library.js
+++ toolings/eslint-config/library.js
@@ -15,7 +15,7 @@ module.exports = {
   extends: [
     "@vercel/style-guide/eslint/node",
     "@vercel/style-guide/eslint/typescript",
-  ].map(require.resolve),
+  ],
   parserOptions: {
     project,
   },
