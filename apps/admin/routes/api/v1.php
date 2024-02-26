3914a4e696285d6d318eb7d7088be0f907b7f848
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-02-26 09:32:04 +0630

    chore: update bun

--- apps/admin/routes/api/v1.php
+++ apps/admin/routes/api/v1.php
@@ -5,9 +5,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-
 Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
-    return $request->user();
+    return new \App\Http\Resources\UserResource($request->user());
 });
 
 
