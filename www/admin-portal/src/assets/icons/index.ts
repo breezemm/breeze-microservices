c7573673f2829a68340bb7041f41a9100c023535
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-02-26 23:48:28 +0630

    feat: add user login feature

--- www/admin-portal/src/assets/icons/index.ts
+++ www/admin-portal/src/assets/icons/index.ts
@@ -3,3 +3,4 @@ export * from './HomeIcon.tsx';
 export * from './WalletIcon.tsx';
 export * from './VerifiedBadgeIcon.tsx';
 export * from './UserProfileIcon.tsx';
+export * from './LogoutIcon.tsx';
--- www/admin-portal/src/routes/_authenticated.tsx
+++ www/admin-portal/src/routes/_authenticated.tsx
@@ -1,4 +1,12 @@
-import {Button, buttonVariants, cn} from '@breeze/ui';
+import {
+  Button,
+  buttonVariants,
+  cn,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuTrigger
+} from '@breeze/ui';
 import {createFileRoute, Link, Outlet, redirect} from '@tanstack/react-router'
 import {authStore} from "~/store";
 import HomeIcon from "~/assets/icons/HomeIcon.tsx";
@@ -6,6 +14,8 @@ import WalletIcon from "~/assets/icons/WalletIcon.tsx";
 import VerifiedBadgeIcon from "~/assets/icons/VerifiedBadgeIcon.tsx";
 import UserProfileIcon from "~/assets/icons/UserProfileIcon.tsx";
 import Logo from '~/assets/icons/Logo';
+import LogoutIcon from "~/assets/icons/LogoutIcon.tsx";
+import {useAuthUser, useSignOutUser} from "~/lib/auth.ts";
 
 
 export const Route = createFileRoute('/_authenticated')({
@@ -20,19 +30,51 @@ export const Route = createFileRoute('/_authenticated')({
   }
 })
 
-function Auth() {
 
-  const links = [
-    {name: "Home", to: "/", icon: HomeIcon},
-    {name: "Wallet", to: "/wallet", icon: WalletIcon},
-    {name: "Verify", to: "/verify", icon: VerifiedBadgeIcon},
-  ]
+const links = [
+  {name: "Home", to: "/", icon: HomeIcon},
+  {name: "Wallet", to: "/wallet", icon: WalletIcon},
+  {name: "Verify", to: "/verify", icon: VerifiedBadgeIcon},
+]
+
+function Auth() {
+  const auth = useAuthUser()
+
+  const signOutUser = useSignOutUser({})
+
+  const onLogout = () => {
+    signOutUser.mutate({})
+  }
+
   return (
     <div className="w-full">
       <header className="flex flex-row justify-between w-full px-10 py-6 border-b">
         <Logo/>
         <div>
-          <UserProfileIcon/>
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <UserProfileIcon/>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-48 absolute right-0 top-1">
+              <DropdownMenuLabel className="w-full text-6 ">
+                {auth.isLoading && "Loading..."}
+                {auth.isSuccess && auth.data?.name}
+              </DropdownMenuLabel>
+              <DropdownMenuLabel>
+                <Button variant={"default"}
+                        className="w-full flex justify-start items-center w-full text-left gap-4 py-2">
+                  <UserProfileIcon className="w-7 h-8"/> My Account</Button>
+              </DropdownMenuLabel>
+              <DropdownMenuLabel>
+                <Button
+                  onClick={onLogout}
+                  variant={"link"}
+                  className="w-full flex justify-start items-center w-full text-destructive text-left gap-4 py-2 hover:no-underline	">
+                  <LogoutIcon/> Logout</Button>
+              </DropdownMenuLabel>
+
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </header>
 
