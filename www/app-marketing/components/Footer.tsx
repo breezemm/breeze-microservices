de32d424c6f7493b4531f4360e77fb740a30d71b
Kaung Myat Thu <kaungmyatthu692017@gmail.com>
2024-03-01 14:42:17 +0630

    fix:fix breeze  logo

--- www/app-marketing/components/Footer.tsx
+++ www/app-marketing/components/Footer.tsx
@@ -1,17 +1,16 @@
-import Breeze from '@/app/assets/water_mark.svg'
-
-import Image from 'next/image'
-import { ContactSection } from '@/app/components/ContactSection'
-import { SocialSection } from '@/app/components/SocialSection'
-import { ContactInfoData, SocialList, Stores } from '@/data/contact'
+import {ContactSection} from '@/app/components/ContactSection'
+import {SocialSection} from '@/app/components/SocialSection'
+import {ContactInfoData, SocialList, Stores} from '@/data/contact'
+import BreezeLogo from '@/app/assets/BreezeLogo'
 
 const Footer = () => {
   return (
     <div className="bg-neutral-10 text-neutral-1 md:px-36 md:pt-10 md:pb-32 px-6 pt-10 pb-20">
-      <Image src={Breeze} alt="water mark" />
-      <div className="mt-7 flex  flex-col gap-10 md:flex-row md:justify-between  ">
-        <ContactSection ContactInfoData={ContactInfoData} />
-        <SocialSection SocialList={SocialList} storesDownload={Stores} />
+
+      <BreezeLogo className='w-36 h-14 m-3 mb-4'/>
+      <div className="  flex  flex-col gap-10 md:flex-row md:justify-between  ">
+        <ContactSection ContactInfoData={ContactInfoData}/>
+        <SocialSection SocialList={SocialList} storesDownload={Stores}/>
       </div>
     </div>
   )
--- www/app-marketing/components/NavBar.tsx
+++ www/app-marketing/components/NavBar.tsx
@@ -1,20 +1,18 @@
-import Image from 'next/image'
 import React from 'react'
-import BreezeIcon from '@/app/assets/breeze_icon.svg'
 import Link from 'next/link'
 import DownloadButton from '@/components/DownloadButton'
+import BreezeLogoIcon from '@/app/assets/BreezeLogoIcon'
 
 const NavBar = () => {
   return (
-    <div className="flex  items-center sticky top-0 z-50  justify-between bg-black   px-6 py-4 md:py-6  md:px-10">
+    <div
+      className="flex  items-center sticky top-0 z-50  justify-between bg-black   ps-3.5 pe-6 py-4 md:py-6  md:pe-10 md:ps-8">
       <Link href="/">
-        <div className="h-10 w-10  md:h-14 md:w-14">
-          <Image src={BreezeIcon} alt="breezicon" />
-        </div>
+        <BreezeLogoIcon className='md:w-20 md:h-20 w-16 h-16'/>
       </Link>
 
       <Link href="/">
-        <DownloadButton />
+        <DownloadButton/>
       </Link>
     </div>
   )
