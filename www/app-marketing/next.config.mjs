9e197305a7d9382d29599b7aceddb0299ffdc681
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-03-06 21:02:11 +0630

    chore(app-marketing): add @breeze/ui

--- www/app-marketing/next.config.mjs
+++ www/app-marketing/next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 import {withContentlayer} from 'next-contentlayer'
 
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: ['@breeze/ui'],
+};
 
 export default withContentlayer(nextConfig);
