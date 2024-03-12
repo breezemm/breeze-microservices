e956844b3f7d1507213239cdf24e24cca9df18b4
Rayy-007 <minkhant88887@gmail.com>
2024-03-12 11:03:30 +0630

    feat: added icons for wallet page

--- www/admin-portal/src/assets/icons/index.ts
+++ www/admin-portal/src/assets/icons/index.ts
@@ -5,3 +5,7 @@ export * from './VerifiedBadgeIcon.tsx';
 export * from './UserProfileIcon.tsx';
 export * from './LogoutIcon.tsx';
 export * from './HomeIcon.tsx';
+export * from './DollarSignIcon.tsx';
+export * from './HistoryIcon.tsx';
+export * from './CloseIcon.tsx';
+export * from './SearchIcon.tsx';
--- www/admin-portal/src/routes/_authenticated/dashboard/wallet/cashinout.tsx
+++ www/admin-portal/src/routes/_authenticated/dashboard/wallet/cashinout.tsx
@@ -1,6 +1,7 @@
 import { Input } from '@breeze/ui'
 import { createFileRoute } from '@tanstack/react-router'
 import UserProfile from './_components/_userprofile'
+import SearchIcon from '~/assets/icons/SearchIcon'
 
 export const Route = createFileRoute('/_authenticated/dashboard/wallet/cashinout')({
   component: CashInOut
@@ -13,20 +14,16 @@ function CashInOut () {
       <div> 
         <div className='my-10'>
           <h5 className="text-lg font-bold leading-6 px-2 mb-6">Search</h5>
-            <div className="relative w-80">
-            <Input
-              type="text"
-              placeholder="Username"
-              className=""
-            />
-
-          <div className="absolute inset-y-0 right-0 flex items-center justify-center pr-[3px]">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-              <circle cx="11.5" cy="11.5" r="9.5" stroke="#323334" stroke-width="1.5" />
-              <path d="M18.5 18.5L22 22" stroke="#323334" stroke-width="1.5" stroke-linecap="round" />
-            </svg>
+          <div className="relative w-80">
+              <Input
+                type="text"
+                placeholder="Username"
+                className=""
+              />
+            <div className="absolute inset-y-0 right-0 flex items-center justify-center pr-[3px]">
+              <SearchIcon />
+            </div>
           </div>
-        </div>
         </div>
 
         <div className="flex flex-col gap-4 px-6 py-1 w-80">
