fcdd1818359bed4e76b95bcbb82148188e7f9f3d
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-03-21 11:54:37 +0630

    chore: add laravel pail

--- apps/wallets/app/Http/Controllers/PaymentController.php
+++ apps/wallets/app/Http/Controllers/PaymentController.php
@@ -7,12 +7,14 @@ use App\Exceptions\InsufficientFundException;
 use App\Models\Wallet;
 use App\Services\WalletService;
 use Cknow\Money\Money;
+use Illuminate\Support\Facades\Log;
 
 class PaymentController extends Controller
 {
     public function __construct(
         public readonly WalletService $walletService,
-    ) {
+    )
+    {
     }
 
     /**
@@ -22,21 +24,31 @@ class PaymentController extends Controller
     public function store(TransactionData $transactionData)
     {
 
-        $from = Wallet::findByUuid($transactionData->from)->first();
+        try {
+            $from = Wallet::findByUuid($transactionData->from)->first();
 
-        $to = Wallet::findByUuid($transactionData->to)->first();
+            $to = Wallet::findByUuid($transactionData->to)->first();
 
-        $amount = Money::MMK($transactionData->amount, true);
+            $amount = Money::MMK($transactionData->amount, true);
 
-        $this->walletService->transfer(
-            from: $from,
-            to: $to,
-            amount: $amount,
-            meta: $transactionData->meta,
-        );
+            $this->walletService->transfer(
+                from: $from,
+                to: $to,
+                amount: $amount,
+                meta: $transactionData->meta,
+            );
 
-        return response()->json([
-            'message' => 'Payment created successfully',
-        ], 201);
+            return response()->json([
+                'message' => 'Payment created successfully',
+            ], 201);
+        } catch (\Exception $e) {
+            Log::error("User {id} payment failed: {$e->getMessage()}", [
+                'id' => $from->uuid,
+            ]);
+            return response()->json([
+                'message' => 'Payment failed',
+            ], 400);
+        }
+
     }
 }
