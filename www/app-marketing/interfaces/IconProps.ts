803a497e360791aff31cd650584c811e77395008
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-03-24 06:52:31 +0630

    fix: remove frotnend

--- www/app-marketing/interfaces/IconProps.ts
+++
@@ -1,4 +0,0 @@
-interface IconPropsInterface {
-  width?: string
-  height?: string
-}
--- www/app-marketing/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-import {withContentlayer} from 'next-contentlayer'
-
-const nextConfig = {
-  transpilePackages: ['@breeze/ui'],
-};
-
-export default withContentlayer(nextConfig);
