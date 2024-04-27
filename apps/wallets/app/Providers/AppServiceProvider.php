e31d106da1b00b247bd7d710946cbaccfed79b0c
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-04-28 02:13:42 +0630

    chore(wallet): remove laravel kafka

--- apps/wallets/app/Providers/AppServiceProvider.php
+++ apps/wallets/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use MyanmarCyberYouths\BreezeSdk\Breeze;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +28,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->bind(
+            Breeze::class,
+            function () {
+                return new Breeze(accessToken: 'dummy token');
+            });
     }
 }
--- apps/wallets/routes/api.php
+++ apps/wallets/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\WalletController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -13,3 +14,4 @@ Route::apiResource('/wallets', WalletController::class)->except('update');
 Route::apiResource('/payments', PaymentController::class)->only('store');
 
 Route::get('/wallets/users/{userId}', [WalletController::class, 'getWalletByUserId']);
+
