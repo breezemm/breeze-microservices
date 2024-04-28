fcd0d9beea5a45b6818d96ad81dc19d98c544480
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-04-29 02:42:40 +0630

    chore: remove dead code

--- apps/wallets/routes/api.php
+++ apps/wallets/routes/api.php
@@ -2,12 +2,7 @@
 
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\WalletController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::apiResource('/wallets', WalletController::class)->except('update');
 Route::apiResource('/payments', PaymentController::class)->only('store');
