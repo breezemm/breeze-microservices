3bc06af8fb0069928624014b1f4143d5a351af4a
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-05-24 02:02:22 +0630

    chore: add .env

--- apps/auth/bootstrap/providers.php
+++ apps/auth/bootstrap/providers.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Providers\AppServiceProvider;
-
 return [
-    AppServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
 ];
