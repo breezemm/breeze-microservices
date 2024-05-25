ae1188ac97acd8f734d00dbe4d2e505aade5b974
Aung Myat Moe <aungmyatmoe834@gmail.com>
2024-05-26 00:24:01 +0630

    chore(posts): add telescope

--- apps/posts/bootstrap/providers.php
+++ apps/posts/bootstrap/providers.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Providers\AppServiceProvider;
+use App\Providers\HorizonServiceProvider;
+
 return [
-    App\Providers\AppServiceProvider::class,
+    AppServiceProvider::class,
+    HorizonServiceProvider::class,
 ];
